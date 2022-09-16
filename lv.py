c32ffe2ce027c68c05597eb926dfdc671aa8890a
lvjiajun <60539121+lv-jiajun@users.noreply.github.com>
2022-09-16 16:00:15 +0800

    Delete lv.py

--- lv.py
+++
@@ -1,3 +0,0 @@
-file = "['FUN1', '(', 'FXint', 'VAR1', ')', '{', 'VAR2', 'FUN2', '(', 'VAR3', ')', ';', 'return', '(', 'VAR4', '*', ')', 'VAR5', '.', 'FUN1', '(', '(', 'void', '*', ')', '(', 'VAR6', ')', 'VAR1', ')', ';', '}']"
-for word in file.split(', '):
-    print(word)
